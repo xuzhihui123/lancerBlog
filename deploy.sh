6843825e491ffc8fa0f77e6f983a033e1d5bc90a
lancer <1271005467@qq.com>
2022-04-03 19:14:31 +0800

    reset

--- deploy.sh
+++ deploy.sh
@@ -7,16 +7,16 @@ set -e
 yarn docs:build
 
 # 进入生成的文件夹
-# cd docs/.vuepress/dist
-
-# 如果是发布到自定义域名
-# echo 'www.example.com' > CNAME
+cd docs/.vuepress/dist
 
 git init
-git add .
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/xuzhihui123/my-blog.git
-git push -u origin main
+git add -A
+git commit -m 'deploy'
+
+# 如果发布到 https://<USERNAME>.github.io
+git push -f git@github.com:xuzhihui123/my-blog.git main
+
+# 如果发布到 https://<USERNAME>.github.io/<REPO>
+# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
 
 cd -
